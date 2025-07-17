e1aabd13448efbd57421b228f30bc5996f9af29f
Andarov <bekturandarob678@gmail.com>
2025-07-17 15:21:32 +0500

    Add site header and extra files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
  export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#028DE3",
+        light: "#171717",
+        dark: "#292a31",
+        gray: { light: "#F5F7F9" },
+      },
+
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
+    },
+
+    screens: {
+      xs: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 }
